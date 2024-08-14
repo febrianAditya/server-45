069fd91565df18d6b01ff09a3a67b22838ffbf1f
FebrianAditya <febrian.aditya@student.upi.edu>
2024-08-14 21:27:24 +0700

    this is initialize server

---
+++ app.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+
+// const express = require("express")
+// const app = express()
+// const port = 3000
+
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "HELLO WORLD GUYS"})
+// })
+
+// app.listen(port => {
+//     console.log("SERVER RUNNING");
+// })
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+//   res.send('Hello World GUYS!')
+    res.status(200).json({ message: "HELLO WORLD GUYS"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
