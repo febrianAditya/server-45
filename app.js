62cb64e01e54f9a99952e0b07c869841dde9da34
FebrianAditya <febrian.aditya@student.upi.edu>
2024-08-16 22:21:51 +0700

    add restart max and delays

--- app.js
+++ app.js
@@ -2,19 +2,6 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
 }
 
-// const express = require("express")
-// const app = express()
-// const port = 3000
-
-
-// app.get("/", (req, res) => {
-//     res.status(200).json({ message: "HELLO WORLD GUYS"})
-// })
-
-// app.listen(port => {
-//     console.log("SERVER RUNNING");
-// })
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     apps : [{
-      name   : "server1",
-      script : "./app.js",
-  env: {
-  PORT: 80
-  }
+        name   : "server1",
+        script : "./app.js",
+        env: {
+            PORT: 80,
+            NODE_ENV: "production"
+        },
+        max_restarts: 10,   // Mengurangi jumlah restart maksimum
+        restart_delay: 5000 // Memberi jeda 5 detik sebelum restart
     }]
   }
   
